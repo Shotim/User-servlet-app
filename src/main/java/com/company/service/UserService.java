97fc72c150dd277241e90d4ac939a4499559c575
shostkotimofei@gmail.com <Timohgako1814>
2019-11-08 14:12:47 +0300

    change functionality

--- src/main/java/com/company/service/UserService.java
+++ src/main/java/com/company/service/UserService.java
@@ -1,4 +1,16 @@
 package com.company.service;
 
+import com.company.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    List<String> findAll();
+
+    String findById(int id);
+
+    void addUser(User user);
+
+    void deleteById(int id);
 }
--- src/main/java/com/company/service/UserServiceImpl.java
+++ src/main/java/com/company/service/UserServiceImpl.java
@@ -1,4 +1,39 @@
 package com.company.service;
 
+import com.company.entity.User;
+import com.company.repository.Repository;
+import com.google.gson.Gson;
+import lombok.Setter;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class UserServiceImpl implements UserService {
+
+    @Setter
+    private Repository repository;
+
+    private Gson gson = new Gson();
+
+    @Override
+    public List<String> findAll() {
+        List<User> users = repository.getAll();
+        return users.stream().map(gson::toJson).collect(toList());
+    }
+
+    @Override
+    public String findById(int id) {
+        return gson.toJson(repository.getById(id));
+    }
+
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
 }
