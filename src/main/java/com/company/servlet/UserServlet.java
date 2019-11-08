d36cdbcb968927e8f80f6bd2f0382cf0b074d2fc
shostkotimofei@gmail.com <Timohgako1814>
2019-11-08 14:13:25 +0300

    add doGet

--- src/main/java/com/company/servlet/UserServlet.java
+++ src/main/java/com/company/servlet/UserServlet.java
@@ -1,9 +1,39 @@
 package com.company.servlet;
 
-import javax.servlet.http.HttpServlet;
+import com.company.service.UserService;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "userServlet", urlPatterns = "users")
 public class UserServlet extends HttpServlet {
 
-    private
+    private UserService service;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        service.findAll().forEach(out::print);
+        out.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
 
 }
