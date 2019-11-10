5c489681a833d0469fae4592832eebd9d73239ff
shostkotimofei@gmail.com <Timohgako1814>
2019-11-10 14:34:47 +0300

    added logger

--- src/main/java/com/company/driver/MySQLDriver.java
+++ src/main/java/com/company/driver/MySQLDriver.java
@@ -1,6 +1,8 @@
 package com.company.driver;
 
 import com.mysql.jdbc.Driver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +18,9 @@ public class MySQLDriver implements com.company.driver.Driver {
     private static Connection connection;
 
     public Connection establishConnection() {
+
+        Logger logger = LoggerFactory.getLogger(MySQLDriver.class);
+        logger.info("Connecting to database...");
         try (InputStream input = MySQLDriver.class.getClassLoader().getResourceAsStream("db.properties")) {
 
             Properties properties = new Properties();
@@ -29,9 +34,12 @@ public class MySQLDriver implements com.company.driver.Driver {
             String password = properties.getProperty("password");
 
             connection = getConnection(url, user, password);
+            logger.info("Connected to My SQL Database");
             registerDriver(new Driver());
 
-        } catch (IOException | SQLException ex) {
+        } catch (SQLException ex) {
+            logger.error("SQL State: {}\n{}", ex.getSQLState(), ex.getMessage());
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return connection;
