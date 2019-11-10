1d7ee92c200491532f8875a9d1201b67b240eb1d
shostkotimofei@gmail.com <Timohgako1814>
2019-11-10 15:23:47 +0300

    Added filter

---
+++ src/main/java/com/company/filter/UserServletFilter.java
@@ -0,0 +1,20 @@
+package com.company.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "userServletFilter", urlPatterns = "/users")
+public class UserServletFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        servletResponse.setCharacterEncoding("UTF-8");
+        servletResponse.setContentType("application/json");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
