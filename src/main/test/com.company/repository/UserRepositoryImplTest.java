80c784f8b28ae57e3cbb4fda5853acbf739419f8
shostkotimofei@gmail.com <Timohgako1814>
2019-11-11 10:55:03 +0300

    modified pom:

--- src/main/test/com.company/repository/UserRepositoryImplTest.java
+++ src/main/test/com.company/repository/UserRepositoryImplTest.java
@@ -24,21 +24,38 @@ class UserRepositoryImplTest {
 
     @org.junit.jupiter.api.Test
     void getById() {
+        User predictable = new User(1, "Name");
+        User actual = repository.getById(1);
+
+        assertEquals(predictable, actual);
     }
 
     @org.junit.jupiter.api.Test
     void getByName() {
+        List<User> predictable = new ArrayList<>();
+        predictable.add(new User(1, "Name"));
+        List<User> actual = repository.getByName("Name");
+        assertEquals(predictable, actual);
     }
 
     @org.junit.jupiter.api.Test
     void addUser() {
+        User predictable = new User(3, "Yura");
+        repository.addUser(predictable);
+        assertEquals(predictable, repository.getById(3));
     }
 
     @org.junit.jupiter.api.Test
     void deleteById() {
+        User predictable = new User();
+        repository.deleteById(3);
+        assertEquals(repository.getById(3), predictable);
     }
 
     @org.junit.jupiter.api.Test
     void updateById() {
+        User predictable = new User(1,"Tima");
+        repository.updateById(predictable);
+        assertEquals(predictable, repository.getById(predictable.getId()));
     }
 }
