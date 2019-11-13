7c69219e17c9a4093ef9a5445938de2ebcf8c4c6
shostkotimofei@gmail.com <Timohgako1814>
2019-11-13 11:53:35 +0300

    Checked style

--- src/main/java/com/leverx/user/driver/DataBaseProperties.java
+++ src/main/java/com/leverx/user/driver/DataBaseProperties.java
@@ -14,6 +14,7 @@ import static lombok.AccessLevel.PRIVATE;
 @FieldDefaults(level = PRIVATE)
 public class DataBaseProperties {
 
+    private static final Logger logger = LoggerFactory.getLogger(DataBaseProperties.class);
     @Getter
     private String DRIVER_CLASS_NAME;
     @Getter
@@ -22,7 +23,6 @@ public class DataBaseProperties {
     private String DATABASE_USERNAME;
     @Getter
     private String DATABASE_PASSWORD;
-    private static final Logger logger = LoggerFactory.getLogger(DataBaseProperties.class);
 
     DataBaseProperties() {
         setCredentials();
