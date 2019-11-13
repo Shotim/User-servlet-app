560af86eb48484195d42ca6683b42a76684cfde8
shostkotimofei@gmail.com <Timohgako1814>
2019-11-13 17:08:12 +0300

    Added status codes to servlet

--- src/main/java/com/leverx/user/servlet/UserServlet.java
+++ src/main/java/com/leverx/user/servlet/UserServlet.java
@@ -14,6 +14,7 @@ import static com.leverx.constants.UserConstants.ID;
 import static com.leverx.utils.ServletUtils.getPathVariable;
 import static com.leverx.utils.ServletUtils.readJsonBody;
 import static java.lang.Integer.parseInt;
+import static javax.servlet.http.HttpServletResponse.*;
 import static org.apache.commons.lang3.math.NumberUtils.isParsable;
 
 @WebServlet(name = "userServlet", urlPatterns = {"/users", "/users/*"})
@@ -40,16 +41,19 @@ public class UserServlet extends HttpServlet {
                     .forEach(out::print);
         }
         out.flush();
+        response.setStatus(SC_OK);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         service.save(readJsonBody(request));
+        response.setStatus(SC_CREATED);
     }
 
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) {
         service.deleteById(request.getParameter(ID));
+        response.setStatus(SC_NO_CONTENT);
     }
 
     @Override
@@ -57,5 +61,6 @@ public class UserServlet extends HttpServlet {
         String id = request.getParameter(ID);
         String user = readJsonBody(request);
         service.updateById(id, user);
+        response.setStatus(SC_CREATED);
     }
 }
