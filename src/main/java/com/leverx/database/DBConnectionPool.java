044706488fd1a40af809da95df9dbd75e1e8e2ff
shostkotimofei@gmail.com <Timohgako1814>
2019-11-14 17:44:06 +0300

    Changed properties names

--- src/main/java/com/leverx/database/DBConnectionPool.java
+++ src/main/java/com/leverx/database/DBConnectionPool.java
@@ -1,30 +1,39 @@
-package com.leverx.database;
+package com.leverx.objectpool;
 
-import com.leverx.objectpool.ObjectPool;
+import com.leverx.database.DataBaseProperties;
 import org.slf4j.Logger;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static java.sql.DriverManager.getConnection;
+import static java.util.Collections.synchronizedList;
+import static java.util.stream.Collectors.toList;
 import static org.slf4j.LoggerFactory.getLogger;
 
-public class DBConnectionPool extends ObjectPool<Connection> {
-    private static final Logger logger = getLogger(DBConnectionPool.class);
-    private DataBaseProperties properties = new DataBaseProperties();
+public class ObjectPool {
 
-    public DBConnectionPool() {
-        super();
-        try {
-            Class.forName(properties.getDriverClassName());
-        } catch (ClassNotFoundException e) {
-            logger.error(e.getMessage());
-        }
-        logger.info("DataBase connection pool was created");
+    private List<Connection> connectionInUse;
+    private List<Connection> connectionOutOfUsage;
+
+    private static final int MAX_POOL_CONNECTION_AMOUNT = 10;
+
+    private DataBaseProperties properties = new DataBaseProperties();
+    private static final Logger logger = getLogger(ObjectPool.class);
+
+    public ObjectPool() {
+        connectionInUse = synchronizedList(new ArrayList<Connection>());
+        connectionOutOfUsage = Stream
+                .generate(this::createConnection)
+                .limit(MAX_POOL_CONNECTION_AMOUNT)
+                .collect(toList());
+        connectionOutOfUsage = synchronizedList(connectionOutOfUsage);
     }
 
-    @Override
-    public Connection create() {
+    private Connection createConnection() {
         try {
             var url = properties.getDatabaseUrl();
             var user = properties.getDatabaseUsername();
@@ -38,25 +47,15 @@ public class DBConnectionPool extends ObjectPool<Connection> {
         }
     }
 
-    @Override
-    public boolean validate(Connection object) {
-        try {
-            boolean isValid = !object.isClosed();
-            logger.info("Connection was validated");
-            return isValid;
-        } catch (SQLException e) {
-            logger.error("SQL state: {}\n{}", e.getSQLState(), e.getMessage());
-            return false;
-        }
+    public Connection startSession() {
+        var connection = connectionOutOfUsage.get(0);
+        connectionInUse.add(connection);
+        return connection;
     }
 
-    @Override
-    public void dead(Connection object) {
-        try {
-            object.close();
-            logger.info("Connection was closed");
-        } catch (SQLException e) {
-            logger.error("SQL state: {}\n{}", e.getSQLState(), e.getMessage());
-        }
+    public void endSession(Connection connection) {
+        connectionInUse.remove(connection);
+        connectionOutOfUsage.add(connection);
     }
+
 }
