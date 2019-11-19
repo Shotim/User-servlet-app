9fffc9005e8b16943e9f67c8f3fb1bcfc58c6491
shostkotimofei@gmail.com <Timohgako1814>
2019-11-19 20:04:40 +0300

    Added owner to Cat

--- src/main/java/com/leverx/cat/entity/Cat.java
+++ src/main/java/com/leverx/cat/entity/Cat.java
@@ -1,5 +1,6 @@
 package com.leverx.cat.entity;
 
+import com.leverx.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
@@ -10,6 +11,7 @@ import static lombok.AccessLevel.PRIVATE;
 @FieldDefaults(level = PRIVATE)
 @AllArgsConstructor
 public class Cat {
+    User owner;
     int id;
     String name;
 }
