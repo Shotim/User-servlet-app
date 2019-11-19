471652a047556fc5c5610b3cccf874bd8c21b6e9
shostkotimofei@gmail.com <Timohgako1814>
2019-11-19 19:52:15 +0300

    Added CatJsonMapper

---
+++ src/main/java/com/leverx/cat/mapper/CatJsonMapper.java
@@ -0,0 +1,28 @@
+package com.leverx.cat.mapper;
+
+import com.google.gson.Gson;
+import com.leverx.cat.entity.Cat;
+import com.leverx.cat.entity.CatDto;
+
+import java.util.Collection;
+
+import static java.util.stream.Collectors.toList;
+
+public class CatJsonMapper {
+
+    private static final Gson gson = new Gson();
+
+    public static String convertFromCatToJson(Cat cat) {
+        return gson.toJson(cat);
+    }
+
+    public static Collection<String> convertFromCatsToJson(Collection<Cat> cats) {
+        return cats.stream()
+                .map(CatJsonMapper::convertFromCatToJson)
+                .collect(toList());
+    }
+
+    public static CatDto convertFromJsonToCayDto(String cat) {
+        return gson.fromJson(cat, CatDto.class);
+    }
+}
