f7b2f7e1c5eccf9110293322a6083252c1bb8fa3
shostkotimofei@gmail.com <Timohgako1814>
2019-11-20 19:01:10 +0300

    Replaced old property loader by new one

--- src/main/java/com/leverx/database/DBConnectionPool.java
+++ src/main/java/com/leverx/database/DBConnectionPool.java
@@ -1,6 +1,5 @@
 package com.leverx.database;
 
-import com.leverx.propertyloader.PropertyLoader;
 import org.slf4j.Logger;
 
 import javax.ws.rs.InternalServerErrorException;
@@ -9,10 +8,6 @@ import java.sql.SQLException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-import static com.leverx.constants.DataBaseCredentialsFields.DRIVER;
-import static com.leverx.constants.DataBaseCredentialsFields.PASSWORD;
-import static com.leverx.constants.DataBaseCredentialsFields.URL;
-import static com.leverx.constants.DataBaseCredentialsFields.USERNAME;
 import static java.sql.DriverManager.getConnection;
 import static java.util.stream.Stream.generate;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -21,11 +16,12 @@ public class DBConnectionPool {
 
     private static final int MAX_POOL_CONNECTION_AMOUNT = 10;
     private static final Logger LOGGER = getLogger(DBConnectionPool.class);
-    private static final PropertyLoader properties = new PropertyLoader();
+    private static DBPropertyLoader propertyLoader;
     private static DBConnectionPool connectionPool;
     private BlockingQueue<Connection> connectionOutOfUsage = new LinkedBlockingQueue<>(MAX_POOL_CONNECTION_AMOUNT);
 
     private DBConnectionPool() {
+        propertyLoader = new DBPropertyLoader();
         addDriver();
         generate(DBConnectionPool::createConnection)
                 .limit(MAX_POOL_CONNECTION_AMOUNT)
@@ -42,9 +38,9 @@ public class DBConnectionPool {
 
     private static Connection createConnection() {
         try {
-            var url = properties.getProperty(URL);
-            var user = properties.getProperty(USERNAME);
-            var password = properties.getProperty(PASSWORD);
+            var url = propertyLoader.getUrl();
+            var user = propertyLoader.getUsername();
+            var password = propertyLoader.getPassword();
 
             var connection = getConnection(url, user, password);
             LOGGER.info("Connection created");
@@ -58,7 +54,7 @@ public class DBConnectionPool {
 
     private void addDriver() {
         try {
-            var driver = properties.getProperty(DRIVER);
+            var driver = propertyLoader.getDriver();
             Class.forName(driver);
         } catch (ClassNotFoundException e) {
             LOGGER.error(e.getMessage());
