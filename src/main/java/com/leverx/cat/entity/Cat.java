b63e64f853fbd7c9cefc09db60c3a57b22799217
shostkotimofei@gmail.com <Timohgako1814>
2019-11-20 11:41:12 +0300

    Added hibernate annotations

--- src/main/java/com/leverx/cat/entity/Cat.java
+++ src/main/java/com/leverx/cat/entity/Cat.java
@@ -3,15 +3,39 @@ package com.leverx.cat.entity;
 import com.leverx.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.AUTO;
 import static lombok.AccessLevel.PRIVATE;
 
 @Data
-@FieldDefaults(level = PRIVATE)
+@NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
+@Entity
+@Table(name = "cats")
 public class Cat {
-    User owner;
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = AUTO)
     int id;
+
+    @Column
     String name;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "ownerId")
+    User owner;
+
 }
--- src/main/java/com/leverx/user/entity/User.java
+++ src/main/java/com/leverx/user/entity/User.java
@@ -1,16 +1,44 @@
 package com.leverx.user.entity;
 
 
+import com.leverx.cat.entity.Cat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+import static javax.persistence.CascadeType.REMOVE;
+import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PRIVATE;
 
 @Data
-@FieldDefaults(level = PRIVATE)
+@NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = IDENTITY)
     int id;
+
+    @Column
     String name;
+
+
+    @OneToMany(fetch = FetchType.LAZY,
+            cascade = REMOVE,
+            mappedBy = "owner")
+    List<Cat> cats;
 }
