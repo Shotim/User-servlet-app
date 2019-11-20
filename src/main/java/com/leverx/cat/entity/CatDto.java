de1605c97fbb5080770d52ac0848056e1f85f6c0
shostkotimofei@gmail.com <Timohgako1814>
2019-11-20 17:56:35 +0300

    Added Validation, fixed bugs

--- src/main/java/com/leverx/cat/entity/CatDto.java
+++ src/main/java/com/leverx/cat/entity/CatDto.java
@@ -4,10 +4,16 @@ package com.leverx.cat.entity;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import static lombok.AccessLevel.PRIVATE;
 
 @Data
 @FieldDefaults(level = PRIVATE)
 public class CatDto {
+
+    @Size(min = 5, max = 60, message = "Name should be between 5 and 60 symbols")
+    @NotNull
     String name;
 }
--- src/main/java/com/leverx/user/entity/UserDto.java
+++ src/main/java/com/leverx/user/entity/UserDto.java
@@ -3,10 +3,16 @@ package com.leverx.user.entity;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import static lombok.AccessLevel.PRIVATE;
 
 @Data
 @FieldDefaults(level = PRIVATE)
 public class UserDto {
+
+    @Size(min = 5, max = 60, message = "Name should be between 5 and 60 symbols")
+    @NotNull
     String name;
 }
