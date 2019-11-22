bbc8512e017bb05f51d40838db5b7df1b3696ecf
shostkotimofei@gmail.com <Timohgako1814>
2019-11-22 18:25:34 +0300

    Added empty methods to repository

---
+++ src/main/java/com/leverx/cat/repository/CatRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.leverx.cat.repository;
+
+import com.leverx.cat.entity.Cat;
+
+import java.util.Collection;
+
+public class CatRepositoryImpl implements CatRepository {
+    @Override
+    public Collection<Cat> findAll() {
+        return null;
+    }
+
+    @Override
+    public Collection<Cat> findByOwner(int ownerId) {
+        return null;
+    }
+
+    @Override
+    public Cat findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Cat save(Cat cat) {
+        return null;
+    }
+}
