a0ef98fe7bd010c0dcd1974ad8c723c99edf5b70
shostkotimofei@gmail.com <Timohgako1814>
2019-11-26 17:40:38 +0300

    Removed orphanRemoval

--- src/main/java/com/leverx/user/entity/User.java
+++ src/main/java/com/leverx/user/entity/User.java
@@ -49,8 +49,7 @@ public class User {
     @JsonInclude(NON_NULL)
     @OneToMany(fetch = EAGER,
             cascade = ALL,
-            mappedBy = "owner",
-            orphanRemoval = true)
+            mappedBy = "owner")
     Collection<Cat> cats;
 }
 
