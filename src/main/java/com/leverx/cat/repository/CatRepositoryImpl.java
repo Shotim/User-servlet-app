c18660d56a7d25b16a7589c8cc29edff59243437
shostkotimofei@gmail.com <Timohgako1814>
2019-11-28 19:33:19 +0300

    Surrounded with try-catch-finally all methods

--- src/main/java/com/leverx/cat/repository/CatRepositoryImpl.java
+++ src/main/java/com/leverx/cat/repository/CatRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.leverx.cat.entity.Cat;
 import org.slf4j.Logger;
 
 import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.InternalServerErrorException;
 import java.util.Collection;
 
 import static com.leverx.config.HibernateConfig.getEntityManagerFactory;
@@ -18,46 +19,70 @@ public class CatRepositoryImpl implements CatRepository {
     @Override
     public Collection<Cat> findAll() {
         var entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        var query = entityManager.createQuery("from Cat");
-        var cats = query.getResultList();
-        entityManager.getTransaction().commit();
-        LOGGER.debug("Were received {} cats", cats.size());
-        entityManager.close();
-        return cats;
+        try {
+            entityManager.getTransaction().begin();
+            var query = entityManager.createQuery("from Cat");
+            var cats = query.getResultList();
+            entityManager.getTransaction().commit();
+            LOGGER.debug("Were received {} cats", cats.size());
+            return cats;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new InternalServerErrorException(e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public Collection<Cat> findByOwner(int ownerId) {
         var entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        var query = entityManager.createQuery("from Cat where owner.id=:ownerId")
-                .setParameter("ownerId", ownerId);
-        var cats = query.getResultList();
-        LOGGER.debug("Were received {} cats with ownerId = {}", cats.size(), ownerId);
-        entityManager.close();
-        return cats;
+        try {
+            entityManager.getTransaction().begin();
+            var query = entityManager.createQuery("from Cat where owner.id=:ownerId")
+                    .setParameter("ownerId", ownerId);
+            var cats = query.getResultList();
+            LOGGER.debug("Were received {} cats with ownerId = {}", cats.size(), ownerId);
+            return cats;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new InternalServerErrorException(e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public Cat findById(int id) {
         var entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        var cat = entityManager.find(Cat.class, id);
-        LOGGER.debug("Was received cat with id = {}", id);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        return cat;
+        try {
+            entityManager.getTransaction().begin();
+            var cat = entityManager.find(Cat.class, id);
+            LOGGER.debug("Was received cat with id = {}", id);
+            entityManager.getTransaction().commit();
+            return cat;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new InternalServerErrorException(e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public Cat save(Cat cat) {
         var entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.persist(cat);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        return cat;
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(cat);
+            entityManager.getTransaction().commit();
+            return cat;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new InternalServerErrorException(e);
+        } finally {
+            entityManager.close();
+        }
     }
 }
--- src/main/java/com/leverx/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/leverx/user/repository/UserRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.leverx.user.entity.User;
 import org.slf4j.Logger;
 
 import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.InternalServerErrorException;
 import java.util.Collection;
 
 import static com.leverx.config.HibernateConfig.getEntityManagerFactory;
@@ -18,71 +19,107 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public Collection<User> findAll() {
         var entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        var query = entityManager.createQuery("from User");
-        var users = query.getResultList();
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        LOGGER.debug("Were received {} users", users.size());
-        return users;
+        try {
+            entityManager.getTransaction().begin();
+            var query = entityManager.createQuery("from User");
+            var users = query.getResultList();
+            entityManager.getTransaction().commit();
+            LOGGER.debug("Were received {} users", users.size());
+            return users;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new InternalServerErrorException(e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public User findById(int id) {
         var entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        var user = entityManager.find(User.class, id);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        LOGGER.debug("User with id = {} was received", id);
-        return user;
+        try {
+            entityManager.getTransaction().begin();
+            var user = entityManager.find(User.class, id);
+            entityManager.getTransaction().commit();
+            LOGGER.debug("User with id = {} was received", id);
+            return user;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new InternalServerErrorException(e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @SuppressWarnings(value = "unchecked")
     @Override
     public Collection<User> findByName(String name) {
         var entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        var query = entityManager.createQuery("from User where name=:name")
-                .setParameter("name", name);
-        var users = query.getResultList();
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        LOGGER.debug("Were received {} users with name = {}", users.size(), name);
-        return users;
+        try {
+            entityManager.getTransaction().begin();
+            var query = entityManager.createQuery("from User where name=:name")
+                    .setParameter("name", name);
+            var users = query.getResultList();
+            entityManager.getTransaction().commit();
+            LOGGER.debug("Were received {} users with name = {}", users.size(), name);
+            return users;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new InternalServerErrorException(e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public User save(User user) {
         var entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.persist(user);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        LOGGER.debug("User was saved");
-        return user;
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(user);
+            entityManager.getTransaction().commit();
+            LOGGER.debug("User was saved");
+            return user;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new InternalServerErrorException(e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void deleteById(int id) {
         var entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        var query = entityManager.createQuery("delete User where id=:id")
-                .setParameter("id", id);
-        query.executeUpdate();
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        LOGGER.debug("User with id = {} was deleted", id);
+        try {
+            entityManager.getTransaction().begin();
+            var query = entityManager.createQuery("delete User where id=:id")
+                    .setParameter("id", id);
+            query.executeUpdate();
+            entityManager.getTransaction().commit();
+            LOGGER.debug("User with id = {} was deleted", id);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new InternalServerErrorException(e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public User update(User user) {
         var entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.merge(user);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        LOGGER.debug("User with id = {} was updated", user.getId());
-        return user;
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(user);
+            entityManager.getTransaction().commit();
+            LOGGER.debug("User with id = {} was updated", user.getId());
+            return user;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new InternalServerErrorException(e);
+        } finally {
+            entityManager.close();
+        }
     }
 }
