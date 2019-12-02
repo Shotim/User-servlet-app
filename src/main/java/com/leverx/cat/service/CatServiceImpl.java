f55dff65391f0ded2ec388ad78bcffab0f31f3a0
shostkotimofei@gmail.com <Timohgako1814>
2019-12-02 15:48:08 +0300

    Refactored code

--- src/main/java/com/leverx/cat/service/CatServiceImpl.java
+++ src/main/java/com/leverx/cat/service/CatServiceImpl.java
@@ -18,20 +18,17 @@ public class CatServiceImpl implements CatService {
 
     @Override
     public Collection<Cat> findAll() {
-        Collection<Cat> cats = catRepository.findAll();
-        return cats;
+        return catRepository.findAll();
     }
 
     @Override
     public Cat findById(int id) {
-        Cat cat = catRepository.findById(id);
-        return cat;
+        return catRepository.findById(id);
     }
 
     @Override
     public Collection<Cat> findByOwner(int ownerId) {
-        Collection<Cat> cats = catRepository.findByOwner(ownerId);
-        return cats;
+        return catRepository.findByOwner(ownerId);
     }
 
     @Override
