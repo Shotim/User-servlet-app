9b7bb969e9cb12cc1fbc4093d59824ee622719f6
shostkotimofei@gmail.com <Timohgako1814>
2019-12-05 19:47:29 +0300

    Fixed bugs

--- src/main/java/com/leverx/cat/dto/CatsDtoIdsList.java
+++ src/main/java/com/leverx/cat/dto/CatsDtoIdsList.java
@@ -3,6 +3,7 @@ package com.leverx.cat.dto;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static lombok.AccessLevel.PRIVATE;
@@ -10,5 +11,5 @@ import static lombok.AccessLevel.PRIVATE;
 @Data
 @FieldDefaults(level = PRIVATE)
 public class CatsDtoIdsList {
-    List<Integer> ids;
+    List<Integer> ids = new ArrayList<>();
 }
