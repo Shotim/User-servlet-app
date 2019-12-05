22b0cc6882f18e75abbf72b44fe03f84b6ae7705
shostkotimofei@gmail.com <Timohgako1814>
2019-12-05 19:51:38 +0300

    Fixed bugs

--- src/main/java/com/leverx/user/dto/UserInputDto.java
+++ src/main/java/com/leverx/user/dto/UserInputDto.java
@@ -5,6 +5,8 @@ import lombok.experimental.FieldDefaults;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static com.leverx.validator.EntityValidator.NOT_VALID_NAME;
 import static lombok.AccessLevel.PRIVATE;
@@ -16,4 +18,6 @@ public class UserInputDto {
     @Size(min = 1, max = 60, message = NOT_VALID_NAME)
     @NotNull
     String name;
+
+    Collection<Integer> catsIdsList = new ArrayList<>();
 }
