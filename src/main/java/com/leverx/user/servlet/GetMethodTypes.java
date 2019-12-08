7d4aacfc49811fbcba56ca53f766acc4dfd85a27
shostkotimofei@gmail.com <Timohgako1814>
2019-12-08 17:42:04 +0300

    Removed unused types

--- src/main/java/com/leverx/user/servlet/GetMethodTypes.java
+++ src/main/java/com/leverx/user/servlet/GetMethodTypes.java
@@ -5,7 +5,6 @@ public enum GetMethodTypes {
     GET_ALL_USERS,
     GET_USER_BY_ID,
     GET_USER_BY_NAME,
-    GET_CATS_OF_USER,
-    GET_CAT_BY_ID_OF_USER
+    GET_CATS_OF_USER
 
 }
--- src/main/java/com/leverx/user/servlet/PutMethodTypes.java
+++ src/main/java/com/leverx/user/servlet/PutMethodTypes.java
@@ -3,6 +3,4 @@ package com.leverx.user.servlet;
 public enum PutMethodTypes {
 
     EDIT_USER,
-    ASSIGN_CATS_TO_USER,
-    MOVE_CAT_TO_ANOTHER_USER
 }
