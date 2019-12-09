15d476d257840bd4163f1144732f9730c06a5b2e
shostkotimofei@gmail.com <Timohgako1814>
2019-12-09 21:28:17 +0300

    Removed method validateUserId()

--- src/main/java/com/leverx/user/validator/UserValidator.java
+++ src/main/java/com/leverx/user/validator/UserValidator.java
@@ -1,26 +1,12 @@
 package com.leverx.user.validator;
 
 import com.leverx.user.dto.UserInputDto;
-import com.leverx.user.repository.UserRepository;
-import com.leverx.user.repository.UserRepositoryImpl;
 import com.leverx.validator.ValidationFailedException;
 
 import static com.leverx.cat.validator.CatValidator.validateCatsIds;
 import static com.leverx.validator.EntityValidator.validate;
 
 public class UserValidator {
-
-    private static final UserRepository USER_REPOSITORY = new UserRepositoryImpl();
-    private static final String USER_DOES_NOT_EXIST = "User does not exist in database";
-
-    public static String validateUserId(Integer id) {
-        var optionalUser = USER_REPOSITORY.findById(id);
-        if (optionalUser.isEmpty()) {
-            var invalidValue = id.toString();
-            return invalidValue + ":  " + USER_DOES_NOT_EXIST;
-        }
-        return "";
-    }
 
     public static void validateUserInputDto(UserInputDto userInputDto) throws ValidationFailedException {
         var errors = validate(userInputDto);
