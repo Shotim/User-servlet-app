bcdae46044de4da45f02ebb00c6d73ff4906e4da
shostkotimofei@gmail.com <Timohgako1814>
2019-12-09 16:02:41 +0300

    Added env variables

--- src/main/java/com/leverx/envvar/constants/DBEnvironmentVariableConstants.java
+++ src/main/java/com/leverx/envvar/constants/DBEnvironmentVariableConstants.java
@@ -6,8 +6,8 @@ public class DBEnvironmentVariableConstants {
     public static final String DB_PASSWORD_ENV = "MYSQL_PASSWORD";
     public static final String DB_URL_ENV = "MYSQL_URL";
 
-    public static final String DB_USERNAME_PROP ="hibernate.connection.username";
-    public static final String DB_PASSWORD_PROP="hibernate.connection.password";
-    public static final String DB_URL_PROP="hibernate.connection.url";
+    public static final String DB_USERNAME_PROP = "hibernate.connection.username";
+    public static final String DB_PASSWORD_PROP = "hibernate.connection.password";
+    public static final String DB_URL_PROP = "hibernate.connection.URL";
 
 }
