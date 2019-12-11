2c15460c180476ab2d9720359b2bec0158172016
shostkotimofei@gmail.com <Timohgako1814>
2019-12-11 11:51:20 +0300

    Fixed logic and check for errors

--- src/main/java/com/leverx/entity/cat/validator/CatValidator.java
+++ src/main/java/com/leverx/entity/cat/validator/CatValidator.java
@@ -1,6 +1,5 @@
 package com.leverx.entity.cat.validator;
 
-import com.leverx.entity.cat.entity.Cat;
 import com.leverx.entity.cat.repository.CatRepository;
 import com.leverx.entity.cat.repository.CatRepositoryImpl;
 
@@ -11,7 +10,6 @@ import java.util.stream.*;
 public class CatValidator {
 
     private static final CatRepository CAT_REPOSITORY = new CatRepositoryImpl();
-    private static final String CAT_HAS_OWNER = "Cat with this id already has owner";
     private static final String CAT_DOES_NOT_EXIST = "Cat with this id does not exist in database";
 
     public static String validateCatsIds(Collection<Integer> catsIds) {
@@ -28,25 +26,12 @@ public class CatValidator {
         if (optionalCat.isEmpty()) {
             return Optional.of(getCatNotExistError(catId));
         } else {
-            if (hasOwner(optionalCat.get())) {
-                return Optional.of(getCatHasOwnerError(catId));
-            }
             return Optional.empty();
         }
-    }
-
-    private static String getCatHasOwnerError(Integer catId) {
-        var invalidValue = catId.toString();
-        return invalidValue + ":  " + CAT_HAS_OWNER;
     }
 
     private static String getCatNotExistError(Integer catId) {
         var invalidValue = catId.toString();
         return invalidValue + ":  " + CAT_DOES_NOT_EXIST;
-    }
-
-    private static boolean hasOwner(Cat cat) {
-        var owners = cat.getOwners();
-        return !owners.isEmpty();
     }
 }
--- src/main/java/com/leverx/entity/dog/validator/DogValidator.java
+++ src/main/java/com/leverx/entity/dog/validator/DogValidator.java
@@ -1,6 +1,5 @@
 package com.leverx.entity.dog.validator;
 
-import com.leverx.entity.dog.entity.Dog;
 import com.leverx.entity.dog.repository.DogRepository;
 import com.leverx.entity.dog.repository.DogRepositoryImpl;
 
@@ -11,7 +10,6 @@ import java.util.stream.*;
 public class DogValidator {
 
     private static final DogRepository DOG_REPOSITORY = new DogRepositoryImpl();
-    private static final String DOG_HAS_OWNER = "Dog with this id already has owner";
     private static final String DOG_DOES_NOT_EXIST = "Dog with this id does not exist in database";
 
     public static String validateDogsIds(Collection<Integer> dogsIds) {
@@ -28,25 +26,12 @@ public class DogValidator {
         if (optionalDog.isEmpty()) {
             return Optional.of(getDogNotExistError(dogId));
         } else {
-            if (hasOwner(optionalDog.get())) {
-                return Optional.of(getDogHasOwnerError(dogId));
-            }
             return Optional.empty();
         }
-    }
-
-    private static String getDogHasOwnerError(Integer dogId) {
-        var invalidValue = dogId.toString();
-        return invalidValue + ":  " + DOG_HAS_OWNER;
     }
 
     private static String getDogNotExistError(Integer dogId) {
         var invalidValue = dogId.toString();
         return invalidValue + ":  " + DOG_DOES_NOT_EXIST;
-    }
-
-    private static boolean hasOwner(Dog dog) {
-        var owners = dog.getOwners();
-        return !owners.isEmpty();
     }
 }
