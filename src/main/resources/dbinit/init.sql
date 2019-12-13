3ef324aad336d85bb104ba144b7356ba5665642f
shostkotimofei@gmail.com <Timohgako1814>
2019-12-13 12:55:49 +0300

    Changed db script

--- src/main/resources/dbinit/init.sql
+++ src/main/resources/dbinit/init.sql
@@ -1,20 +1,49 @@
-create database if not exists users;
-use users;
-create table if not exists users
+create table pets
 (
-    id   int auto_increment
+    id          int auto_increment
         primary key,
-    name text not null
+    name        varchar(60) not null,
+    dateOfBirth date        not null,
+    ownerId     int         null
 );
 
 create table cats
 (
-    id          int auto_increment
-        primary key,
-    name        text null,
-    ownerId     int  null,
-    dateOfBirth date null,
-    constraint cats_users_id_fk
-        foreign key (ownerId) references users (id)
+    catId            int null,
+    miceCaughtNumber int not null,
+    constraint cats_pets_id_fk
+        foreign key (catId) references pets (id)
             on update cascade on delete cascade
 );
+
+create table dogs
+(
+    dogId     int        null,
+    isCutEars tinyint(1) not null,
+    constraint dogs_pets_id_fk
+        foreign key (dogId) references pets (id)
+            on update cascade on delete cascade
+);
+
+create index pets_users_id_fk
+    on pets (ownerId);
+
+create table users
+(
+    id   int auto_increment
+        primary key,
+    name varchar(60) not null
+);
+
+create table user_pet
+(
+    userId int not null,
+    petId  int not null,
+    constraint user_pet_pets_id_fk
+        foreign key (petId) references pets (id)
+            on update cascade on delete cascade,
+    constraint user_pet_users_id_fk
+        foreign key (userId) references users (id)
+            on update cascade on delete cascade
+);
+
