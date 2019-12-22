37612f01d8d51beec876ff8eb63462a55a3e60e5
shostkotimofei@gmail.com <Timohgako1814>
2019-12-22 15:25:49 +0300

    Added new Messages for validation

--- src/main/java/com/leverx/bundle/BundleConstants.java
+++ src/main/java/com/leverx/bundle/BundleConstants.java
@@ -9,6 +9,8 @@ public class BundleConstants {
     public static final String CAT_DOES_NOT_EXIST = "CAT_DOES_NOT_EXIST";
     public static final String DOG_DOES_NOT_EXIST = "DOG_DOES_NOT_EXIST";
     public static final String USER_NOT_FOUND = "USER_NOT_FOUND";
+    public static final String EQUAL_SENDER_AND_RECIPIENT = "EQUAL_SENDER_AND_RECIPIENT";
+    public static final String NOT_ENOUGH_MONEY = "NOT_ENOUGH_MONEY";
 
     public static String getLocalizedMessage(String constantName) {
         return ResourceBundle.getBundle(FILE_NAME, Locale.getDefault()).getString(constantName);
